04a0aaaa8a9fc25d35e8217626ceb8e2e689eaf0
McClain-Thiel <mcclain.thiel@gmail.com>
2019-11-15 15:33:22 -0800

    updating the GAN.py file

--- GAN.py
+++ GAN.py
@@ -2,11 +2,16 @@ import tensorflow as tf
 from tensorflow.keras.layers import LeakyReLU, Reshape, Conv2DTranspose, Conv2D
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import MaxPooling2D, Dropout, Dense, Flatten, BatchNormalization
+import time
+
 
 class GAN(object):
     def __init__(self):
         self.artist = self.build_artist()
         self.critic = self.build_critic()
+        self.artist_opt = tf.keras.optimizers.Adam(1e-4)
+        self.critic_opt = tf.keras.optimizers.Adam(1e-4)
+
 
     def build_artist(self):
         model = Sequential([
@@ -53,6 +58,11 @@ class GAN(object):
         return cross_entropy(tf.ones_like(fake_output), fake_output)
 
     def train_step(self, images):
+        #refactor
+        generator = self.artist
+        discriminator = self.critic
+        BATCH_SIZE = 16
+        noise_dim = 100
 
         noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
@@ -62,11 +72,36 @@ class GAN(object):
             real_output = discriminator(images, training=True)
             fake_output = discriminator(generated_images, training=True)
 
-            gen_loss = generator_loss(fake_output)
-            disc_loss = discriminator_loss(real_output, fake_output)
+            gen_loss = self.generator_loss(fake_output)
+            disc_loss = self.discriminator_loss(real_output, fake_output)
 
         gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
         gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
-        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+        self.artist_opt.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+        self.critic_opt.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+    def train(self, dataset, epochs):
+        for epoch in range(epochs):
+            start = time.time()
+
+            for image_batch in dataset:
+                train_step(image_batch)
+
+            # Produce images for the GIF as we go
+            display.clear_output(wait=True)
+            generate_and_save_images(generator,
+                                     epoch + 1,
+                                     seed)
+
+            # Save the model every 15 epochs
+            if (epoch + 1) % 15 == 0:
+                checkpoint.save(file_prefix=checkpoint_prefix)
+
+            print('Time for epoch {} is {} sec'.format(epoch + 1, time.time() - start))
+
+        # Generate after the final epoch
+        display.clear_output(wait=True)
+        generate_and_save_images(generator,
+                                 epochs,
+                                 seed)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import sys
 import os
 import data
 import recognition
+import GAN
+import tensorflow as tf
+
 
 
 
@@ -26,8 +29,9 @@ if __name__ =='__main__':
     print('Tensorflow Version: ', tf.__version__)
     directory = sys.argv[1:][0]
     print(directory[0])
-    data = data.Data(directory[0])
-    train_gen, val_gen = data.train_datagen, data.test_datagen
-    recognition_model = recognition.Recognition()
-    recognition_model.train_and_visualize(train_gen, val_gen)
+    #data = data.Data(directory[0])
+    #train_gen, val_gen = data.train_datagen, data.test_datagen
+    #recognition_model = recognition.Recognition()
+    #recognition_model.train_and_visualize(train_gen, val_gen)
+    test = GAN.GAN()
 
